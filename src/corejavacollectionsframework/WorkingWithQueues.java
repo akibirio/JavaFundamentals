327f19b46989d37d9a4b39dffb1087641364fc8e
AKIBIRIO <ayubkibirio@gmail.com>
2022-12-20 12:44:03 +0300

    Added implementation of the Queue Interface using LinkedList class.

---
+++ src/corejavacollectionsframework/WorkingWithQueues.java
@@ -0,0 +1,28 @@
+package corejavacollectionsframework;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class WorkingWithQueues {
+    public static void main(String[] args) {
+
+        Queue<Person> supermarketCustomers = new LinkedList<>();
+        supermarketCustomers.add(new Person("Alex", 21));
+        supermarketCustomers.add(new Person("Miriam", 18));
+        supermarketCustomers.add(new Person("Ali", 40));
+
+        //get the size of the queue.
+        System.out.println("Size of Queue: " + supermarketCustomers.size());
+        System.out.println("Who is at the Front: " + supermarketCustomers.peek());  // return an element and don't remove.
+        System.out.println(supermarketCustomers.poll());  // return the front element and remove it.
+        System.out.println("Size of Queue after Pool: " + supermarketCustomers.size());
+
+        
+    }
+
+    // a record to define an Object that will be inserted in the queue
+    static record Person(String name, int age) {
+    }
+    
+}
