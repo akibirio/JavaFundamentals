8173710fe334742341c1f3cb1b2123f232420830
AKIBIRIO <ayubkibirio@gmail.com>
2022-12-20 13:46:56 +0300

    Adde implementation of teh set interface using the HashSet Class. HashSet is backed by the HashMap class of Map interface.

---
+++ src/corejavacollectionsframework/JavaSets.java
@@ -0,0 +1,21 @@
+package corejavacollectionsframework;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class JavaSets {
+    public static void main(String[] args) {
+        Set<Integer> numbers = new HashSet<>();
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(2);
+
+        Iterator<Integer> iterate = numbers.iterator();
+        while (iterate.hasNext()) {
+            System.out.println(iterate.next());            
+        }
+    }
+    
+}
